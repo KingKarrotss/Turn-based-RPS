8f1f94a9d0efb54960c6b55133103f0daca62bf8
mbuyt0n <shusrith03@gmail.com>
2025-04-07 22:45:16 +0530

    training

--- training/pointer.c
+++ training/pointer.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+// int main()
+// {
+//     int a[] = {1, 2, 5, 4, 5};
+//     int *p = a;
+//     for(int i = 0; i < 5; i++)
+//     {
+//         printf("%d\n", *(p + i));
+//     }
+// }
+
+
+void swap(int *p1, int *p2)
+{
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    swap(&a, &b);
+    printf("%d,%d\n", a, b);
+}
